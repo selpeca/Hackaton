9909cc5fedcbd223612e178946e5ad2d50f38f45
Sergio Elías Pérez Camargo <ser.per.eli@gmail.com>
2024-10-05 18:06:38 -0500

    feat: add people model and migration

---
+++ hackaton/backend/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using shared.Entities;
+
+namespace backend.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<People> People { get; set; }
+}
---
+++ hackaton/shared/Entities/People.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shared.Entities;
+
+public class People
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Nombre")]
+    [MaxLength(100, ErrorMessage = "EL nombre debe ser menor a 100 caracteres")]
+    [Required]
+    public string Name { get; set; }
+
+    [Display(Name = "Apellido")]
+    [MaxLength(100, ErrorMessage = "EL apellido debe ser menor a 100 caracteres")]
+    [Required]
+    public string LastName { get; set; }
+
+    [Display(Name = "Fecha de nacimiento")]
+    public DateTime BirthDate { get; set; }
+
+    [Display(Name = "Tipo de documento")]
+    public string TypeDocument { get; set; }
+
+    [Display(Name = "Número de documento")]
+    public string Document { get; set; }
+
+    [Display(Name = "Nombre completo")]
+    public string FullName => $"{Name}  {LastName}";
+}
